8d4b7cc48e5df0fed49904ff710983976e6d19e4
yacchiDAO <okamura@net.lab.uec.ac.jp>
2020-04-18 23:57:57 +0900

    set nuxt for githubpage

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,11 @@
 
+// `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
+const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
+  router: {
+    base: '/yacchi_portfolio/'
+  }
+} : {}
+
 export default {
   mode: 'universal',
   /*
@@ -50,5 +57,6 @@ export default {
     */
     extend (config, ctx) {
     }
-  }
+  },
+  routerBase
 }
